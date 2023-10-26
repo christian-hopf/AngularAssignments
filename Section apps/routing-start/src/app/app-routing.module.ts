18d67c40143ea60b0d6456d81eb48ea0db47f730
christian-hopf <christian.hopf@cognizant.com>
2023-10-26 15:40:59 -0500

    Section 11 routing: navigation guard, resolve guard, static and dynamic route data

--- Section apps/routing-start/src/app/app-routing.module.ts
+++ Section apps/routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,8 +20,16 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      {
+        path: ':id',
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard],
+      },
     ],
   },
   {
@@ -26,11 +37,17 @@ const appRoutes: Routes = [
     component: UsersComponent,
     children: [{ path: ':id/:name', component: UserComponent }],
   },
-  { path: 'not-found', component: NotFoundComponent },
+  // { path: 'not-found', component: NotFoundComponent },
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: { error: 'Page not found' },
+  },
   { path: '**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
 })
